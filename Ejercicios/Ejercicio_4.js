94077f393bd08a6b9601d86234dc64ba47e8079d
juan <juanes29061215@gmail.com>
2024-12-06 07:40:48 -0500

    Ejercicio 4

---
+++ Ejercicios/Ejercicio_4.js
@@ -0,0 +1,9 @@
+ // Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par
+ // o impar
+
+ let numero = parseInt(prompt("Ingrese un numero"));
+ if (numero % 2 === 0) {
+     console.log("El numero es par");
+ } else {
+     console.log("El numero es impar");
+ }

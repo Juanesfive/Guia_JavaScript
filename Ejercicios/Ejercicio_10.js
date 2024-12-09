90a321c9f9e50ef1a322373daa0bb3bfea59a880
juan <juanes29061215@gmail.com>
2024-12-09 10:26:44 -0500

    Ejercicio 11

--- Ejercicios/Ejercicio_10.js
+++ Ejercicios/Ejercicio_10.js
@@ -7,7 +7,7 @@
 // Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en
 // función de su respuesta le muestre un menú con los ingredientes disponibles para que
 // elija. Solo se puede elegir un ingrediente además de la mozzarella y el tomate que están 
-// en todas las pizzas. Al final se debe mostrar por pantalla si la pizza elegida es vegetariana
+// en todas las pizzas. Al final se debe mostrar por pantalla si la pizza elegida es veget
 // o no y todos los ingredientes que lleva.
 
 let pizza = prompt("¿Desea una pizza vegetariana? (si/no)");
---
+++ Ejercicios/Ejercicio_11.js
@@ -0,0 +1,21 @@
+// Escribir un programa que sume, resta, multiplique y divida dos números
+
+let num1 = parseInt(prompt("ingrese un numero"));
+
+let num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+let suma = num1 + num2;
+
+let resta = num1 - num2;
+
+let multiplicacion = num1 * num2;
+
+let dividir = num1 / num2;
+
+alert(`El resultado de la suma es: ${suma}`);
+
+alert(`El resultado de la resta es: ${resta}`);
+
+alert(`El resultado de la multiplicacion es: ${multiplicacion}`);
+
+alert(`El resultado de la division es: ${dividir} `);

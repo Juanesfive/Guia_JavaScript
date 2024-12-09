533f34aed0ad3baa0272d50454c98e83450ef90a
juan <juanes29061215@gmail.com>
2024-12-09 10:43:47 -0500

    Ejercicio 12

---
+++ Ejercicios/Ejercicio_12.js
@@ -0,0 +1,8 @@
+// Escribir un programa que calcule el área de un rectángulo:
+
+
+let largo = parseInt(prompt("Ingrese el largo del rectángulo"));
+ 
+let ancho = parseInt(prompt("Ingrese el ancho del rectangulo"));
+
+alert(`El área de este rectangulo teniendo en cuenta que el largo es ${largo} y el ancho es ${ancho} es: ` + (largo * ancho));

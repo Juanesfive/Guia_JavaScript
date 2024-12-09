399db8c8980982145b8606e40e252dd1f5e47833
juan <juanes29061215@gmail.com>
2024-12-09 11:29:51 -0500

    Ejercicio 14

---
+++ Ejercicios/Ejercicio_14.js
@@ -0,0 +1,7 @@
+// Escribir un programa que calcule el área de un triángulo.
+
+let base = parseInt(prompt("Ingresa la base del triángulo"));
+
+let altura = parseInt(prompt("Ingresa la altura del triangulo"));
+
+alert(`El área de este triangulo teniendo en cuenta que la base es ${base} y la altura es ${altura} es: ` + ( 1/2 * base * altura ));

99892994ec45a78c27322933946ccd4fd12da52f
juan <juanes29061215@gmail.com>
2024-12-09 11:06:56 -0500

    Ejercicio 13

---
+++ Ejercicios/Ejercicio_13.js
@@ -0,0 +1,11 @@
+// Escribir un programa que lea 4 números y calcule la media.
+
+let num1 = parseInt(prompt("Ingrese un numero"));
+
+let num2 = parseInt(prompt("Ingrese un segundo numero"));
+
+let num3 = parseInt(prompt("Ingrese un tercer numero"));
+
+let num4 = parseInt(prompt("Ingrese un cuarto numero"));
+
+alert(`La media de ${num1}, ${num2}, ${num3} y ${num4} es ` + (num1 + num2 + num3 + num4)/4)

b8569ed437e42a991b6ac6cfc012053fa28e80d7
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 17:08:16 -0500

    Ejercicio 39

---
+++ Ejercicios/Ejercicio_39.js
@@ -0,0 +1,8 @@
+// Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura
+// Considere que se cobra por m2 y realice el código que representen el algoritmo que le
+// permita ir generando presupuestos para cada cliente.
+
+let m2 = 10000;
+let cantidadM2 = parseInt(prompt("Ingrese la cantidad de m2 que se va a pintar"));
+
+alert(`El costo del trabajo es de: ` + (m2 * cantidadM2));

642fe602aabf737eb3367e1b34f66650549515a7
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 17:38:07 -0500

    Ejercicio 47

---
+++ Ejercicios/Ejercicio_47.js
@@ -0,0 +1,26 @@
+// Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el
+// menor o si son iguales.
+
+let num1 = parseInt(prompt("Ingrese un numero"));
+let num2 = parseInt(prompt("Ingrese un segundo numero"));
+let num3 = parseInt(prompt("Ingrese un tercer numero"));
+
+if (num1 > num2 && num1 > num3) {
+    alert("El numero mayor es: " + num1);
+} else if (num2 > num1 && num2 > num3) {
+    alert("El numero mayor es: " + num2);
+} else if (num3 > num1 && num3 > num2) {
+    alert("El numero mayor es: " + num3);
+}
+
+if (num1 < num2 && num1 < num3) {
+    alert("El numero menor es: " + num1);
+} else if (num2 < num1 && num2 < num3) {
+    alert("El numero menor es: " + num2);
+} else if (num3 < num1 && num3 < num2) {
+    alert("El numero menor es: " + num3);
+}
+
+if (num1 === num2 && num1 === num3) {
+    alert("Los numeros son iguales");
+}   

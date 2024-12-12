5258a574cf0351194b7abb102e866b22936687fb
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 16:51:38 -0500

    Ejercicio 33

---
+++ Ejercicios/Ejercicio_33.js
@@ -0,0 +1,7 @@
+// Calcular la edad de una madre en el momento que dio a luz a alguno de sus hijos.
+
+let edad = parseInt(prompt("Ingrese la edad de la madre"));
+
+let edadHijo = parseInt(prompt("Ingrese la edad del hijo"));
+
+alert(`La edad de la madre es: ` + (edad - edadHijo));

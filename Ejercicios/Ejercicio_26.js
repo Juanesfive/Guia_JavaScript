c1a7dbcf2ca864a17bf9e85dfd469a8014f5e94a
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 16:10:57 -0500

    Ejercicio 26

---
+++ Ejercicios/Ejercicio_26.js
@@ -0,0 +1,5 @@
+// Hacer un diagrama para convertir de grados centígrados a grados Fahrenheit
+
+let grados = parseInt(prompt("Ingrese los grados centígrados"));
+
+alert(`Los grados Fahrenheit son: ` + ((grados * 9/5) + 32));

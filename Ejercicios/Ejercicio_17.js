c2963adc7dfdb18ac238f09516bc711dd788c0b0
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 14:59:43 -0500

    Ejercicio 17

---
+++ Ejercicios/Ejercicio_17.js
@@ -0,0 +1,7 @@
+// Escribir un programa que calcule el volumen de una esfera.
+
+let radio = parseInt(prompt("Ingrese el radio de la esfera"));
+
+
+
+alert(`El volumen de la esfera es: ` + (4/3 * Math.PI * radio * radio * radio));

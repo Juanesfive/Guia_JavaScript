b46c68569a432d2f642b818d17c30767bca67249
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 17:03:20 -0500

    Ejercicio 37

---
+++ Ejercicios/Ejercicio_37.js
@@ -0,0 +1,8 @@
+// Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿cuántos
+// Kg transporta?
+
+let pesoCajas = 748;
+let pesoCamion = 25;
+let pesoTotal = pesoCajas * pesoCamion;
+
+alert(`El peso total que transporta el camion es de: ` + pesoTotal);

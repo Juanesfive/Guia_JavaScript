f7f8a66428fedb4b25ad1e7abf9850426c70abc3
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 16:05:33 -0500

    Ejercicio 23

---
+++ Ejercicios/Ejercicio_23.js
@@ -0,0 +1,6 @@
+// Realice un algoritmo que, a partir de proporcionarle la velocidad de un automóvil
+// expresada en kilómetros por hora, proporcione la velocidad en metros por segundo.
+
+let velocidad = parseInt(prompt("Ingrese la velocidad del automovil"));
+
+alert(`La velocidad en metros por segundo es: ` + (velocidad * 1000));

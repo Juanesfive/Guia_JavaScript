8c18bcdbda8b664cdef2ef84a2866e5964465f3f
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 14:47:22 -0500

    Ejercicio 16

---
+++ Ejercicios/Ejercicio_16.js
@@ -0,0 +1,9 @@
+// Escribir un programa que calcule la velocidad de un proyectil que recorre 2km en 5
+// minutos, exprese el resultado en metros/segundo.
+
+let distancia = 2000;
+let tiempo = 5*60;
+
+
+
+alert(`La velocidad del proyectil es: `  + (distancia / tiempo) + " m/s");

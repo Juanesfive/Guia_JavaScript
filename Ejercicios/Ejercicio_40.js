3a6fc691a307211b72963acc69f7ec135ae00d5c
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 17:10:57 -0500

    Ejercicio 40

---
+++ Ejercicios/Ejercicio_40.js
@@ -0,0 +1,9 @@
+// Una empresa que contrata personal requiere determinar la edad de las personas que
+// solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año en que
+// nacieron. Realice el código que representen el algoritmo para solucionar este problema.
+
+let añoNacimiento = parseInt(prompt("Ingrese el año de nacimiento"));
+
+let edad = 2024 - añoNacimiento;
+
+alert(`La edad de la persona es: ` + edad);

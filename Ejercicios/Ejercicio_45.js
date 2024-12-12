8f17e3b52b066f37453bafaabaaa417906875dfd
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 17:32:44 -0500

    Ejercicio 45

---
+++ Ejercicios/Ejercicio_45.js
@@ -0,0 +1,9 @@
+// Determinar si un número es positivo y menor que 100.
+
+let numero = parseInt(prompt("Ingrese un numero"));
+
+if (numero > 0 && numero < 100) {
+    alert("El numero es positivo y menor que 100");
+} else {
+    alert("El numero no es positivo y menor que 100");
+}

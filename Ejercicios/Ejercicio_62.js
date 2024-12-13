208a93fe1a2bc9026a44ac5c1d63dc7ce3de854e
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 19:17:51 -0500

    Ejercicio 62

---
+++ Ejercicios/Ejercicio_62.js
@@ -0,0 +1,7 @@
+// Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces
+
+let palabra = prompt("Ingrese una palabra");
+
+for (let i = 0; i < 10; i++) {
+    console.log(`${i + 1}:  ${palabra}`);
+}

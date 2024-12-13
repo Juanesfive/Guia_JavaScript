fe1d9b4c98162dd4b5840edcb7e077d039c3dd54
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 21:19:12 -0500

    Ejercicio 80

---
+++ Ejercicios/Ejercicio_80.js
@@ -0,0 +1,10 @@
+// Crea una función que genere la tabla de multiplicar del número 5, no se debe mostrar 5
+// * 5, nos debe mostrar el resultado para las demás multiplicaciones.
+
+function tablaMultiplicar() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(` ${5 * i}`);
+  }
+}
+
+tablaMultiplicar();

fb11eb98e5fe7f88a7b32e2ca2c3f9b10265406c
Juan Vasquez <juanes29061215@gmail.com>
2024-12-12 20:18:30 -0500

    Ejercicio 74

--- Ejercicios/Ejercicio_74.js
+++ Ejercicios/Ejercicio_74.js
@@ -1,1 +1,9 @@
- 
+// Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el
+// usuario escriba “salir” que terminará.
+
+
+let texto = "";
+while (texto !== "salir") {
+    texto = prompt("Ingrese un texto");
+    console.log(texto);
+}
